468c378f7ceda2149eaa52063f88ea68de4e8a26
soumya098 <soumyarj904@gmail.com>
2022-03-13 21:41:26 +0530

    directory corrected

--- dist/DayNightToogle/js/index.js
+++
@@ -1,6 +0,0 @@
-const inp = document.querySelector("#switch");
-const body = document.querySelector("#overview");
-console.log(inp);
-inp.addEventListener("change", () => {
-	body.classList.toggle("dark");
-});
