b58cea56aad5777c202c468ed9fcbe87b7b30ff6
soumya098 <soumyarj904@gmail.com>
2022-03-31 13:52:43 +0530

    config chnages

--- modals/modals.js
+++ modals/modals.js
@@ -1,13 +1,11 @@
 $(function () {
+	// $("#myModal1").modal("show");
 	$(".modal").draggable({
 		handle: ".modal-header",
 	});
 
 	$(".modal").on("shown.bs.modal", function (e) {
-		$(".modal-body iframe").attr(
-			"src",
-			"https://www.youtube.com/embed/8zcfIu3tyw4?autoplay=0&amp;modestbranding=1&amp;showinfo=0&amp;start=0"
-		);
+		$(".modal-body iframe").attr("src", "https://www.youtube.com/embed/8zcfIu3tyw4?autoplay=0&amp;modestbranding=1&amp;showinfo=0&amp;start=0");
 	});
 
 	$(".modal").on("hide.bs.modal", function (e) {
