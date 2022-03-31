19690ded1b6f644fea651655433b6bd26656ac2a
soumya098 <soumyarj904@gmail.com>
2022-03-31 12:47:55 +0530

    created a draggable div

--- modals/modals.js
+++ modals/modals.js
@@ -14,3 +14,24 @@ $(function () {
 		$(".modal iframe").attr("src", " ");
 	});
 });
+
+const wrapper = document.querySelector(".wrapper");
+const header = wrapper.querySelector(".header");
+
+function onDrag({ movementX, movementY }) {
+	let getStyle = window.getComputedStyle(wrapper);
+	let left = parseInt(getStyle.left); //getting left value of wrapper;
+	let top = parseInt(getStyle.top); //getting top value of wrapper;
+	wrapper.style.left = `${left + movementX}px`;
+	wrapper.style.top = `${top + movementY}px`;
+}
+
+header.addEventListener("mousedown", () => {
+	header.classList.add("active");
+	header.addEventListener("mousemove", onDrag);
+});
+
+document.addEventListener("mouseup", () => {
+	header.classList.remove("active");
+	header.removeEventListener("mousemove", onDrag);
+});
